bdf79af71e74b80ebe9c631345fcb275c010e824
PaulSebastianKrus <paulsebastiankrus@gmail.com>
2024-10-04 11:07:16 +0200

    test for activity done

--- backend/backend/src/main/resources/h2init.sql
+++ backend/backend/src/main/resources/h2init.sql
@@ -1,9 +1,13 @@
 DROP TABLE IF EXISTS activities;
-CREATE TABLE activities (
-                            activity_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                            name VARCHAR(50) NOT NULL UNIQUE,
-                            description TEXT,
-                            weightlimit BIGINT NOT NULL,
-                            agelimit BIGINT NOT NULL,
-                            season VARCHAR(100) NOT NULL
+CREATE TABLE activities
+(
+    activity_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name        VARCHAR(50)  NOT NULL UNIQUE,
+    description TEXT,
+    weightlimit BIGINT       NOT NULL,
+    agelimit    BIGINT       NOT NULL,
+    season      VARCHAR(100) NOT NULL
 );
+INSERT INTO activities (activity_id, name, description, weightlimit, agelimit, season)
+VALUES (1, 'testActivity1', 'testDescription1', 10, 18, 'testSommer1'),
+       (2, 'testActivity2', 'testDescription2', 11, 19, 'testWinter2');
--- backend/backend/src/test/java/org/example/backend/ActivityRepositoryH2Test.java
+++ backend/backend/src/test/java/org/example/backend/ActivityRepositoryH2Test.java
@@ -6,11 +6,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -24,6 +25,7 @@ public class ActivityRepositoryH2Test {
     @Test
     public void addActivity() {
         Activity newActivity = new Activity();
+
         newActivity.setActivityName("testActivity");
         newActivity.setDescription("testDescription");
         newActivity.setWeightLimit(11L);
@@ -32,8 +34,63 @@ public class ActivityRepositoryH2Test {
 
         repository.addActivity(newActivity);
 
-        Activity retrievedActivity = repository.getActivityById(1L);
+        Activity retrievedActivity = repository.getActivityById(3L);
 
         assertEquals("testActivity", retrievedActivity.getActivityName());
     }
+
+
+    @Test
+    public void editActivity(){
+        Activity existingActivity = repository.getActivityById(1L);
+
+        assertNotNull(existingActivity);
+
+        existingActivity.setActivityName("updatedActivity");
+        existingActivity.setDescription("updatedDescription");
+        existingActivity.setWeightLimit(70L);
+        existingActivity.setAgeLimit(18L);
+        existingActivity.setSeason("updatedSeason");
+
+        Activity updatedActivity = repository.updateActivity(existingActivity);
+
+        assertNotNull(updatedActivity);
+        assertEquals("updatedActivity", updatedActivity.getActivityName());
+        assertEquals("updatedDescription", updatedActivity.getDescription());
+        assertEquals(70L, updatedActivity.getWeightLimit());
+        assertEquals(18L, updatedActivity.getAgeLimit());
+        assertEquals("updatedSeason", updatedActivity.getSeason());
+
+    }
+
+    @Test
+    public void deleteActivity(){
+
+        Activity existingActivity = repository.getActivityById(2L);
+
+        assertNotNull(existingActivity);
+
+        repository.deleteActivity(existingActivity.getActivity_id());
+
+        Activity deletedActivity = null;
+        try {
+            deletedActivity = repository.getActivityById(existingActivity.getActivity_id());
+        } catch (EmptyResultDataAccessException e) {
+        }
+
+        assertNull(deletedActivity);
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
 }
